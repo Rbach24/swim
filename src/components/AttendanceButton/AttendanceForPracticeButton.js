ee608e5d8fc7d7e17d8d4845071ce47f2db92cff
Rbach24 <96384502+Rbach24@users.noreply.github.com>
2023-10-01 15:57:45 -0400

    Update AttendanceForPracticeButton.js

--- src/components/AttendanceButton/AttendanceForPracticeButton.js
+++ src/components/AttendanceButton/AttendanceForPracticeButton.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { TouchableHighlight, Image, Text, View } from 'react-native';
 import PropTypes from 'prop-types';
 import stylessss from './styles';
-
+//oct 1
 export default function AttendanceForPracticeButton (props) {
     return (
       <TouchableHighlight underlayColor='rgba(73,182,77,0.9)' onPress={props.onPress } >
