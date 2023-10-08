c70c93d63e3fbd4f358594eb1b906a1b65dc00eb
Rbach24 <96384502+Rbach24@users.noreply.github.com>
2023-10-08 11:44:04 -0400

    Create schedule.js

---
+++ src/screens/Schedule/schedule.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { TextInput, View, StyleSheet, Text} from "react-native";
+
+export default function Schedule(props) {
+    return(
+        //had an error here displaying text because I didn't import text and I didn't have return()
+    <View>
+        <Text style = {{textAlign : "center"}}>Coming Soon... </Text>
+    </View>
+    );
+    
+}
